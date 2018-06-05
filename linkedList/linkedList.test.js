de7874c6cfe261aa795fe89e6e81e80fb04c0bdc
John Blaine <jablaine@uw.edu>
2018-06-05 12:09:55 -0600

    Add linkedList toy problem

---
+++ linkedList/linkedList.test.js
@@ -0,0 +1,43 @@
+
+describe('LinkedList', function() {
+  it('should exist', function() {
+
+  });
+
+  it('should be a function', function() {
+
+  });
+
+  it('should be implemented using the pseudoclassical pattern', function() {
+
+  });
+
+  it('should designate a new tail when new nodes are added', function() {
+
+  });
+
+  it('should remove the head from the list when removeHead is called', function() {
+
+  });
+
+  it('should remove the head and tail from the list when removeHead is called and there is only one element present', function() {
+
+  });
+
+  it('should return the value of the removed head when removeHead is called', function() {
+
+  });
+
+  it('should return null when removeHead is called on an empty list', function() {
+
+  });
+
+  it('should contain a value that was added', function() {
+
+  });
+
+  it('should not contain a value that was removed', function() {
+
+  });
+
+});
