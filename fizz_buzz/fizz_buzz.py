1d7f45a2944e70684922c859b8fbb64dfc825e6b
John Blaine <jablaine@uw.edu>
2018-11-24 05:04:23 -0700

    Finish basic fizz buzz algorithm

--- fizz_buzz/fizz_buzz.py
+++ fizz_buzz/fizz_buzz.py
@@ -1,13 +1,13 @@
-number = input("Please enter an integer to check if it is fizzy or buzzy:")
+number = int(input("Please enter an integer to check if it is fizzy or buzzy:"))
 
-isFizzy = number % 3 == 0
-isBuzzy = number % 5 == 0
+is_fizzy = number % 3 == 0
+is_buzzy = number % 5 == 0
 
-if isFizzy && isBuzzy:
+if is_fizzy and is_buzzy:
     print("This number is both fizzy and buzzy!")
-elif isFizzy:
+elif is_fizzy:
     print("This number is fizzy!")
-elif isBuzzy:
+elif is_buzzy:
     print("This number is buzzy!")
 else:
     print("This number is neither fizzy nor buzzy!")
